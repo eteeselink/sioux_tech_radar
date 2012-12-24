5ea0747edb461bc4f8221d3b626df38311c66673
Egbert Teeselink <e@teeselink.nl>
2012-12-24 17:09:40 +0100

    Disconnected domain from range; radar range is now hard-coded to a radius of 200 pixels; domain is whatever pixel side the radar class gets given. Also, this is starting to look like an actual radar!

--- bootstrap/js/tech-radar.js
+++ bootstrap/js/tech-radar.js
@@ -18,6 +18,9 @@ var Quadrant = (function () {
     Quadrant.prototype.angleUpper = function () {
         return this.angle + deg45;
     };
+    Quadrant.prototype.isLeft = function () {
+        return Math.abs(this.angle) < (Math.PI / 2);
+    };
     return Quadrant;
 })();
 var D3Node = (function () {
@@ -34,18 +37,13 @@ var D3Link = (function () {
     }
     return D3Link;
 })();
-function random(from, to) {
-    var domain = to - from;
-    return Math.random() * domain + from;
-}
 var Thing = (function (_super) {
     __extends(Thing, _super);
-    function Thing(name, quadrant, goodness, scale) {
+    function Thing(name, quadrant, goodness) {
         _super.call(this, null, null);
         this.name = name;
         this.quadrant = quadrant;
-        this.scale = scale;
-        var r = goodness * scale;
+        var r = goodness * Radar.radius;
         var phi = random(quadrant.angleLower(), quadrant.angleUpper());
         this.polar = new Polar(r, phi);
         this.updateXY();
@@ -67,7 +65,7 @@ var Thing = (function (_super) {
         return this.fixed & 2;
     };
     Thing.prototype.goodness = function () {
-        return this.polar.r / this.scale;
+        return this.polar.r / Radar.radius;
     };
     return Thing;
 })(D3Node);
@@ -88,76 +86,114 @@ var Polar = (function () {
     };
     return Polar;
 })();
+function random(from, to) {
+    var domain = to - from;
+    return Math.random() * domain + from;
+}
 function cap(lowerBound, value, upperBound) {
     return Math.max(lowerBound, Math.min(upperBound, value));
 }
-var Viewport = (function () {
-    function Viewport(width) {
+var Radar = (function () {
+    function Radar(width) {
         this.width = width;
         var _this = this;
-        this.quadrantGravity = 0.05;
-        this.svg = d3.select("body").append("svg").attr("width", width * 1.5).attr("height", width).style("border", "1px solid black");
-        this.force = d3.layout.force().nodes([]).size([
-            this.width, 
+        this.quadrantGravity = 0.03;
+        var halfWidth = this.width / 2;
+        this.transformx = d3.scale.linear().domain([
+            -Radar.radius, 
+            Radar.radius
+        ]).range([
+            halfWidth, 
+            3 * halfWidth
+        ]);
+        this.transformy = d3.scale.linear().domain([
+            -Radar.radius, 
+            Radar.radius
+        ]).range([
+            0, 
             this.width
-        ]).gravity(0).charge(-200).on("tick", function (e) {
+        ]);
+        this.scale = d3.scale.linear().domain([
+            0, 
+            Radar.radius * 2
+        ]).range([
+            0, 
+            this.width
+        ]);
+        this.svg = d3.select("body").append("svg").attr("width", width * 2).attr("height", width);
+        this.drawBackground();
+        this.force = d3.layout.force().nodes([]).size([
+            Radar.radius * 2, 
+            Radar.radius * 2
+        ]).gravity(0).charge(-100).on("tick", function (e) {
             return _this.tick(e);
         });
         this.things = this.force.nodes();
     }
-    Viewport.prototype.tick = function (e) {
+    Radar.radius = 200;
+    Radar.prototype.tick = function (e) {
+        var _this = this;
         for(var i = 0; i != this.things.length; i++) {
             var thing = this.things[i];
             thing.updatePolar();
             thing.fixRadius();
             thing.polar.phi += (thing.quadrant.angle - thing.polar.phi) * e.alpha * this.quadrantGravity;
-            thing.polar.phi = cap(thing.quadrant.angleLower(), thing.polar.phi, thing.quadrant.angleUpper());
-            thing.polar.r = Math.min(thing.polar.r, this.width / 2);
+            var borderOffset = 10 / (thing.polar.r + 0.1);
+            thing.polar.phi = cap(thing.quadrant.angleLower() + borderOffset, thing.polar.phi, thing.quadrant.angleUpper() - borderOffset);
+            thing.polar.r = Math.min(thing.polar.r, Radar.radius);
             thing.updateXY();
         }
         var origin = this.width / 2;
-        this.svg.selectAll("circle").attr("cx", function (thing) {
-            return thing.x + origin;
+        this.svg.selectAll("circle.thing").attr("cx", function (thing) {
+            return _this.transformx(thing.x);
         }).attr("cy", function (thing) {
-            return thing.y + origin;
+            return _this.transformy(thing.y);
         });
-        this.svg.selectAll("text").attr("x", function (thing) {
-            return thing.x + origin;
+        this.svg.selectAll("text.thing").attr("x", function (thing) {
+            return _this.transformx(thing.x);
         }).attr("y", function (thing) {
-            return thing.y + origin;
+            return _this.transformy(thing.y);
         }).text(function (thing) {
             return thing.name + " (" + thing.goodness().toPrecision(2) + ")";
         });
     };
-    Viewport.prototype.addThing = function (thing) {
+    Radar.prototype.drawBackground = function () {
+        this.svg.append("line").attr("x1", this.transformx(0)).attr("y1", this.transformy(Radar.radius)).attr("x2", this.transformx(0)).attr("y2", this.transformy(-Radar.radius)).attr("stroke", "black").attr("stoke-width", 1);
+        this.svg.append("line").attr("x1", this.transformx(Radar.radius)).attr("y1", this.transformy(0)).attr("x2", this.transformx(-Radar.radius)).attr("y2", this.transformy(0)).attr("stroke", "black").attr("stoke-width", 1);
+        this.svg.append("circle").attr("cx", this.transformx(0)).attr("cy", this.transformy(0)).attr("r", this.scale(Radar.radius / 2)).attr("stroke", "black").attr("stroke-width", 1).attr("fill", "none");
+    };
+    Radar.prototype.addThing = function (thing) {
         this.things.push(thing);
     };
-    Viewport.prototype.restart = function () {
-        var circles = this.svg.selectAll("circle").data(this.things);
+    Radar.prototype.restart = function () {
+        var circles = this.svg.selectAll("circle.thing").data(this.things);
         var enter = circles.enter().append("circle");
-        enter.attr("r", 10).attr("fill", "#3366ff").call(this.force.drag);
-        var texts = this.svg.selectAll("text").data(this.things).enter().append("text").attr("dx", 20).attr("dy", 5);
+        enter.attr("class", "thing").attr("r", 10).attr("fill", "#3366ff").call(this.force.drag);
+        var texts = this.svg.selectAll("text.thing").data(this.things).enter().append("text").attr("class", "thing").attr("dx", function (thing) {
+            return thing.quadrant.isLeft() ? 20 : -20;
+        }).attr("dy", 5).attr("text-anchor", function (thing) {
+            return thing.quadrant.isLeft() ? "start" : "end";
+        });
         this.force.start();
     };
-    return Viewport;
+    return Radar;
 })();
 $(function () {
-    var width = 200;
     var things = [
-        new Thing("C++", Quadrant.Languages, 0.9, width), 
-        new Thing("Scala", Quadrant.Languages, 0.6, width), 
-        new Thing("TypeScript", Quadrant.Languages, 0.7, width), 
-        new Thing("C#", Quadrant.Languages, 0.1, width), 
-        new Thing("APL", Quadrant.Languages, 0.8, width), 
-        new Thing("Continuous Integration", Quadrant.Techniques, 0.8, width), 
-        new Thing("CodeSourcery GCC", Quadrant.Platforms, 0.5, width), 
-        new Thing("NCrunch", Quadrant.Tools, 0.5, width), 
-        new Thing("Git", Quadrant.Tools, 0.6, width), 
+        new Thing("C++", Quadrant.Languages, 0.9), 
+        new Thing("Scala", Quadrant.Languages, 0.6), 
+        new Thing("TypeScript", Quadrant.Languages, 0.7), 
+        new Thing("C#", Quadrant.Languages, 0.1), 
+        new Thing("APL", Quadrant.Languages, 0.8), 
+        new Thing("Continuous Integration", Quadrant.Techniques, 0.8), 
+        new Thing("CodeSourcery GCC", Quadrant.Platforms, 0.5), 
+        new Thing("NCrunch", Quadrant.Tools, 0.5), 
+        new Thing("Git", Quadrant.Tools, 0.6), 
         
     ];
-    var canvas = new Viewport(width * 2);
+    var radar = new Radar(400);
     for(var i = 0; i != things.length; i++) {
-        canvas.addThing(things[i]);
+        radar.addThing(things[i]);
     }
-    canvas.restart();
+    radar.restart();
 });
--- bootstrap/js/tech-radar.ts
+++ bootstrap/js/tech-radar.ts
@@ -21,6 +21,10 @@ class Quadrant {
   public angleUpper() {
     return this.angle + deg45;
   }
+
+  public isLeft() {
+    return Math.abs(this.angle) < (Math.PI / 2);
+  }
 }
 
 class D3Node {
@@ -35,20 +39,14 @@ class D3Link {
 }
 
 
-function random(from: number, to: number) {
-  var domain = to - from;
-  return Math.random() * domain + from;
-}
-
 class Thing extends D3Node {
   constructor(
     public name: string, 
     public quadrant: Quadrant, 
     goodness: number,   // between 0.0 and 1.0; closer to zero is better
-    private scale: number,
   ) {
     super(null, null);
-    var r = goodness * scale;
+    var r = goodness * Radar.radius;
     
     var phi = random(quadrant.angleLower(), quadrant.angleUpper());
     this.polar = new Polar(r, phi);
@@ -78,7 +76,7 @@ class Thing extends D3Node {
     return this.fixed & 2;
   }
   public goodness() {
-    return this.polar.r / this.scale;
+    return this.polar.r / Radar.radius;
   }
 }
 
@@ -98,34 +96,66 @@ class Polar {
   }
 }
 
+
+function random(from: number, to: number) {
+  var domain = to - from;
+  return Math.random() * domain + from;
+}
+
 function cap(lowerBound: number, value: number, upperBound: number) {
   return Math.max(lowerBound, Math.min(upperBound, value));
 }
 
 
-class Viewport {
+class Radar {
   constructor(
     private width: number,
   ) {
+
+    //// create transforms and other plot-related constants
+
+    var halfWidth = this.width / 2;
+
+    this.transformx = d3.scale.linear()
+      .domain([-Radar.radius, Radar.radius])
+      .range([halfWidth, 3 * halfWidth]);
+
+    this.transformy = d3.scale.linear()
+      .domain([-Radar.radius, Radar.radius])
+      .range([0, this.width]);
+
+    this.scale = d3.scale.linear()
+      .domain([0, Radar.radius * 2])
+      .range([0, this.width]);
+
     this.svg = d3.select("body").append("svg")
-      .attr("width", width * 1.5)
-      .attr("height", width)
-      .style("border", "1px solid black");
+      .attr("width", width * 2)
+      .attr("height", width);
+
+    this.drawBackground();
 
     this.force = d3.layout.force()
       .nodes([])
-      .size([this.width, this.width])
+      .size([Radar.radius * 2, Radar.radius * 2])
       .gravity(0)
-      .charge(-200)
+      .charge(-100)
       .on("tick", e => this.tick(e));
 
     this.things = <Thing[]>this.force.nodes();
   }
 
+  // the distance from the origin to each of the four edges
+  // i.e. the radar's radius. hard-coded to 200 because some of the
+  // force's constants depend on it
+  public static radius = 200;
+
+  private transformx: (number) => number;
+  private transformy: (number) => number;
+  private scale: (number) => number;
   private svg: any;
   private force: any;
   private things: Thing[];
-  private quadrantGravity = 0.05;
+  private quadrantGravity = 0.03;
   
   private tick(e: any) {
     
@@ -135,80 +165,119 @@ class Viewport {
     for (var i = 0; i != this.things.length; i++) {
       var thing = this.things[i];
       
+      // "read" the newly computed x,y values into `thing.polar`.
       thing.updatePolar();
+
+      // set the thing's radius to whatever it was, unless we're being dragged
       thing.fixRadius();
       
-      //console.log(e.alpha);
+      // enable "quadrant gravity", pulling each node a bit to the centre diagonal
+      // of its quadrant
       thing.polar.phi += (thing.quadrant.angle - thing.polar.phi) * e.alpha * this.quadrantGravity;
 
-      thing.polar.phi = cap(thing.quadrant.angleLower(), thing.polar.phi, thing.quadrant.angleUpper());
+      var borderOffset = 10 / (thing.polar.r + 0.1);
+      // ensure that nodes never leave their quadrant
+      thing.polar.phi = cap(thing.quadrant.angleLower() + borderOffset, thing.polar.phi, thing.quadrant.angleUpper() - borderOffset);
 
-      thing.polar.r = Math.min(thing.polar.r, this.width / 2);
+      // ensure that nodes never leave the radar
+      thing.polar.r = Math.min(thing.polar.r, Radar.radius);
 
-      //if(thing.name.indexOf("cala")===1) console.log(thing.polar.phi);
+      // "save" the modifed polar coordinates back to x,y.
       thing.updateXY();
     }
 
     var origin = this.width / 2;
 
-    this.svg.selectAll("circle")
-      .attr("cx", thing => thing.x + origin)
-      .attr("cy", thing => thing.y + origin);
+    this.svg.selectAll("circle.thing")
+      .attr("cx", thing => this.transformx(thing.x))
+      .attr("cy", thing => this.transformy(thing.y));
 
-    this.svg.selectAll("text")
-      .attr("x", thing => thing.x + origin)
-      .attr("y", thing => thing.y + origin)
+    this.svg.selectAll("text.thing")
+      .attr("x", thing => this.transformx(thing.x))
+      .attr("y", thing => this.transformy(thing.y))
       .text(thing => thing.name + " (" + thing.goodness().toPrecision(2) + ")");
+  }
+
+  private drawBackground() {
+
+    // x axis
+    this.svg.append("line")
+      .attr("x1", this.transformx(0))
+      .attr("y1", this.transformy(Radar.radius))
+      .attr("x2", this.transformx(0))
+      .attr("y2", this.transformy(-Radar.radius)) 
+      .attr("stroke", "black")
+      .attr("stoke-width", 1);
+
+    // y axis
+    this.svg.append("line")
+      .attr("x1", this.transformx(Radar.radius))
+      .attr("y1", this.transformy(0))
+      .attr("x2", this.transformx(-Radar.radius))
+      .attr("y2", this.transformy(0)) 
+      .attr("stroke", "black")
+      .attr("stoke-width", 1);
+
+    this.svg.append("circle")
+      .attr("cx", this.transformx(0))
+      .attr("cy", this.transformy(0))
+      .attr("r", this.scale(Radar.radius / 2))
+      .attr("stroke", "black")
+      .attr("stroke-width", 1)
+      .attr("fill", "none");
   }
 
   public addThing(thing: Thing) {
     this.things.push(thing);
   }
 
-  
+  // restart the force animation. re-call this every time you add one or
+  // more Things to the radar  .
   public restart() {
 
-    var circles = this.svg.selectAll("circle")
+    var circles = this.svg.selectAll("circle.thing")
         .data(this.things);
     
-    // enter; no special animation; create sub-elements upon creation
+    // the enter set; no special animation; create sub-elements upon creation
     var enter = circles.enter().append("circle");
     
-    
     enter
+      .attr("class", "thing")
       .attr("r", 10)
       .attr("fill", "#3366ff")
       .call(this.force.drag);
 
-    var texts = this.svg.selectAll("text")
+    var texts = this.svg.selectAll("text.thing")
       .data(this.things)
       .enter()
       .append("text")
-      .attr("dx", 20)
+      .attr("class", "thing")
+      .attr("dx", (thing: Thing) => thing.quadrant.isLeft() ? 20 : -20)
       .attr("dy", 5)
+      .attr("text-anchor", (thing: Thing) => thing.quadrant.isLeft() ? "start" : "end")
     
     this.force.start();
   }
 }
 
 $(function() {
-  var width = 200;
+  
 
   var things = [
-    new Thing("C++",        Quadrant.Languages, 0.9, width),
-    new Thing("Scala",      Quadrant.Languages, 0.6, width),
-    new Thing("TypeScript", Quadrant.Languages, 0.7, width),
-    new Thing("C#",         Quadrant.Languages, 0.1, width),
-    new Thing("APL",        Quadrant.Languages, 0.8, width),
-    new Thing("Continuous Integration", Quadrant.Techniques, 0.8, width),
-    new Thing("CodeSourcery GCC", Quadrant.Platforms, 0.5, width),
-    new Thing("NCrunch", Quadrant.Tools, 0.5, width),
-    new Thing("Git", Quadrant.Tools, 0.6, width),
+    new Thing("C++",        Quadrant.Languages, 0.9),
+    new Thing("Scala",      Quadrant.Languages, 0.6),
+    new Thing("TypeScript", Quadrant.Languages, 0.7),
+    new Thing("C#",         Quadrant.Languages, 0.1),
+    new Thing("APL",        Quadrant.Languages, 0.8),
+    new Thing("Continuous Integration", Quadrant.Techniques, 0.8),
+    new Thing("CodeSourcery GCC", Quadrant.Platforms, 0.5),
+    new Thing("NCrunch", Quadrant.Tools, 0.5),
+    new Thing("Git", Quadrant.Tools, 0.6),
   ];
 
-  var canvas = new Viewport(width * 2);
+  var radar = new Radar(400);
   for (var i = 0; i != things.length; i++) {
-    canvas.addThing(things[i]);
+    radar.addThing(things[i]);
   }
-  canvas.restart();
+  radar.restart();
 });
