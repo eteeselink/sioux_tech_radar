e6d69ce22f1ea37f20232ba3a72dea7997702807
Ronald Crooy <r.a.crooy@gmail.com>
2013-04-03 22:02:56 +0200

    fixed serve.sh for when nginx was not yet running (i.e. when restart does not work)

--- serve.sh
+++ serve.sh
@@ -2,5 +2,10 @@
 # Absolute path to this script. 
 SCRIPT=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/`basename "${BASH_SOURCE[0]}"`
 NGINXDATA=`dirname $SCRIPT`/nginx-data
-nginx -p $NGINXDATA -c $NGINXDATA/conf/nginx.conf -s reload
+if [ -e "/usr/loc/var/run/nginx.pid" ] 
+then
+	nginx -p $NGINXDATA -c $NGINXDATA/conf/nginx.conf -s reload
+else
+	nginx -p $NGINXDATA -c $NGINXDATA/conf/nginx.conf 
+fi
 echo "now goto http://localhost:54321"
