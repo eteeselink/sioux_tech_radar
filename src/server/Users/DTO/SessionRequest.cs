c4b3362a2ee1271348bac62c8cd58fbe31b345c1
Egbert Teeselink <skrebbel@gmail.com>
2013-06-11 16:07:23 +0200

    Made a start on SessionService, which manages logging in and out

---
+++ src/server/Users/DTO/SessionRequest.cs
@@ -0,0 +1,15 @@
+﻿using ServiceStack.ServiceHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sioux.TechRadar.Users.DTO
+{
+    [Route("/api/session", "POST")]
+    public class SessionRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ src/server/Users/SessionService.cs
@@ -0,0 +1,21 @@
+﻿using NLog;
+using ServiceStack.ServiceInterface;
+using Sioux.TechRadar.Users.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sioux.TechRadar.Users
+{
+    public class SessionService : Service
+    {
+        private static Logger logger = LogManager.GetLogger("SessionService");
+        public IThingsRepository Repository { get; set; }  //Injected by IOC
+
+        public void Post(SessionRequest sessionRequest)
+        {
+
+        }
+    }
+}
