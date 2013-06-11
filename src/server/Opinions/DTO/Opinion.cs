ba38f7dc38e6c6b274835a74c4eaf64dfd6b85a9
Ronald Crooy <ronald.crooy@sioux.eu>
2013-06-11 11:29:35 +0200

    simplified opinion object, change accessibility of repository

--- src/server/Opinions/DTO/Opinion.cs
+++ src/server/Opinions/DTO/Opinion.cs
@@ -6,14 +6,11 @@ using ServiceStack.DataAnnotations;
 
 namespace Sioux.TechRadar
 {
-    [Route("/api/opinions/{name}", "POST")] // update
-    [Route("/api/opinions", "PUT")] // create    
+    [Route("/api/opinions/{thingName}", "POST")] // update
+    [Route("/api/opinions", "PUT")] // create        
     public class Opinion : IReturn<Opinion>
     {
-        [PrimaryKey]
-        [AutoIncrement]
-        public int Id { get; set; }
-        public Thing thing { get; set; }
+        public String thingName{ get; set; }
         public int goodness { get; set; }
     }
 }
--- src/server/Opinions/IOpinionsRepository.cs
+++ src/server/Opinions/IOpinionsRepository.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sioux.TechRadar.Opinions
+{
+    public class IOpinionsRepository
+    {
+        public object StoreUpdated(Opinion opinion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object StoreNew(Opinion opinion)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
