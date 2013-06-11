6c64e36d5fbb23af19e90105bdfa1c32d1380c36
Ronald Crooy <ronald.crooy@sioux.eu>
2013-06-11 11:01:32 +0200

    adding Opionions to Server

---
+++ src/server/Opinions/DTO/Opinion.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Linq;
+using ServiceStack.ServiceHost;
+using ServiceStack.DataAnnotations;
+
+namespace Sioux.TechRadar
+{
+    [Route("/api/opinions/{name}", "POST")] // update
+    [Route("/api/opinions", "PUT")] // create    
+    public class Opinion : IReturn<Opinion>
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int Id { get; set; }
+        public Thing thing { get; set; }
+        public int goodness { get; set; }
+    }
+}
---
+++ src/server/Opinions/OpinionsService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using ServiceStack.ServiceInterface;
+using NLog;
+using ServiceStack.Common.Web;
+using System.Collections.Generic;
+using System.Net;
+using Sioux.TechRadar.Opinions;
+
+namespace Sioux.TechRadar
+{
+    public class OpinionsService: Service
+    {
+        private static Logger logger = LogManager.GetLogger("OpinionsService");
+        public IOpinionsRepository Repository { get; set; }  //Injected by IOC
+
+        public object Post(Opinion opinion)
+        {
+            Console.WriteLine("Post received opinion string = " + opinion.ToString());
+            return Repository.StoreUpdated(opinion);
+        }
+
+        public object Put(Opinion opinion)
+        {
+            Console.WriteLine("Put received opinion string = " + opinion.ToString());
+            return Repository.StoreNew(opinion);       
+        }
+    }
+}
